7e1ee12b630eedc0f8e80844eaf564590181ea24
ІП-43 Дутов Іван Андрійович <dutov.ivan-ip43@edu.kpi.ua>
2026-05-09 02:25:56 +0300

    ci: deploy works

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
